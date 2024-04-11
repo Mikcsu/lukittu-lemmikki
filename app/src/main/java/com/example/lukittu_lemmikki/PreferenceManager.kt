4beb324f4681264d8eeda31b72c6ed6bdd7e67f1
HannuOikarinen <oikarhan@gmail.com>
2024-04-11 11:07:42 +0300

    creating preference manager for saved data

---
+++ app/src/main/java/com/example/lukittu_lemmikki/PreferenceManager.kt
@@ -0,0 +1,42 @@
+package com.example.lukittu_lemmikki
+
+import android.content.Context
+
+class PreferencesManager(context: Context) {
+    private val sharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+
+    fun saveProgress(progress: Float) {
+        val editor = sharedPreferences.edit()
+        editor.putFloat("progress", progress)
+        editor.apply()
+    }
+    fun addProgress(additionalProgress: Float) {
+        val currentProgress = getProgress()
+        val newProgress = currentProgress + additionalProgress
+        saveProgress(newProgress)
+    }
+
+    fun getProgress(): Float {
+        return sharedPreferences.getFloat("progress", 0.0f)
+    }
+
+    fun saveSteps(steps: Int) {
+        val editor = sharedPreferences.edit()
+        editor.putInt("Steps", steps)
+        editor.apply()
+    }
+
+    fun getSteps(): Int {
+        return sharedPreferences.getInt("Steps", 0)
+    }
+
+    fun saveSelectedModel(model: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("SelectedModel", model)
+        editor.apply()
+    }
+
+    fun getSelectedModel(): String? {
+        return sharedPreferences.getString("SelectedModel", null)
+    }
+}
